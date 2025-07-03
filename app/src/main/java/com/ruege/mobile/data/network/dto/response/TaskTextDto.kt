be18421a92b599ee2a049f08d144052e0cb392a1
aVoKaD-0 <139006972+aVoKaD-0@users.noreply.github.com>
2025-07-03 15:44:10 +0500

    сейчас будет обновление, до 14:30!

--- app/src/main/java/com/ruege/mobile/data/network/dto/response/TaskTextDto.kt
+++
@@ -1,9 +0,0 @@
-package com.ruege.mobile.data.network.dto.response
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class TaskTextDto(
-    @Json(name = "text") val text: String?
-) 
--- app/src/main/java/com/ruege/mobile/utilss/AuthEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.ruege.mobile.utilss
-
-sealed class AuthEvent {
-    object SessionExpired : AuthEvent()
-} 
